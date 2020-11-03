3bccd473dfc9bf10f05a9ea989e5c08bb6af40f2
Mario Feix <mfguerrero@msn.com>
2020-11-02 21:33:03 -0700

    CollectionPreview and CollectionItem components added

--- src/App.js
+++ src/App.js
@@ -2,22 +2,14 @@ import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-
-export const HatsPage = () => {
-  return (
-    <div>
-      HatS Page
-    </div>
-  )
-}
-
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/shop/hats" component={HatsPage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
---
+++ src/components/preview-collection/collection-preview.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { PropTypes } from 'prop-types'
+
+import CollectionItem from '../collection-item/collection-item.component';
+import './collection-preview.styles.scss';
+
+const CollectionPreview = ({ title, items }) => {
+  return (
+    <div className="collection-preview">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <div className="preview">
+        {
+          items.filter((item, idx) => idx < 4).map(({ id, ...rest }) => (
+            <CollectionItem key={id} {...rest} />
+          ))
+        }
+      </div>
+    </div>
+  )
+}
+
+CollectionPreview.propTypes = {
+  title: PropTypes.string.isRequired,
+  items: PropTypes.array.isRequired
+}
+
+export default CollectionPreview;
+
+
