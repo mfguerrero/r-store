7f8b1997959d6a6127773d9764fb5c7f4a8c18b5
Mario Feix <mfguerrero@msn.com>
2020-11-06 11:35:59 -0700

    Shop and directory data added to redux state

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,30 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect'
 
+import { selectCollections } from '../../redux/shop/shop.selectors';
 import CollectionPreview from '../../components/preview-collection/collection-preview.component';
-import { SHOP_DATA } from './shop-data';
 
-export class ShopPage extends Component {
-  constructor() {
-    super();
-    this.state = {
-      collections: SHOP_DATA
-    }
-  }
+export const ShopPage = ({ collections }) => {
 
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {
-          collections.map(({ id, ...rest }) => (
-            <CollectionPreview
-              key={id}
-              {...rest}
-            />))
-        }
-      </div>
-    )
-  }
+  return (
+    <div className="shop-page">
+      {
+        collections.map(({ id, ...rest }) => (
+          <CollectionPreview
+            key={id}
+            {...rest}
+          />))
+      }
+    </div>
+  )
+
 }
 
-export default ShopPage;
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections
+})
+
+export default connect(mapStateToProps)(ShopPage);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+import { persistStore } from 'redux-persist'
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 import rootReducer from './root-reducer';
@@ -7,6 +8,8 @@ import rootReducer from './root-reducer';
 const middleware = [logger];
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middleware)));
+
+export const persistor = persistStore(store);
 
 export default store;
 
