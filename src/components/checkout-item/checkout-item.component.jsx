4509c58bf07555f76e0f50fb0173b0e85ba5f715
Mario Feix <mfguerrero@msn.com>
2020-11-05 22:50:13 -0700

    added add item, remove item functionality in cart

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,19 +1,33 @@
 import React from 'react'
+import { connect } from 'react-redux';
 
+import { addItem, removeItem, clearItem } from '../../redux/cart/cart.actions'
 import './checkout-item.styles.scss';
 
-const CheckoutItem = ({ item: { imageUrl, price, name, quantity }, addItem }) => {
+
+const CheckoutItem = ({ item, addItem, removeItem, clearItem }) => {
+  const { id, imageUrl, price, name, quantity } = item;
   return (
     <div className="checkout-item">
       <div className="image-container">
         <img src={imageUrl} alt="item" />
       </div>
       <span className="name">{name}</span>
-      <span className="quantity">{quantity}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={() => removeItem(item)}>&#10094;</div>
+        <span className="value">{quantity}</span>
+        <div className="arrow" onClick={() => addItem(item)}>&#10095;</div>
+      </span>
       <span className="price">{price}</span>
-      <div className="remove-button">&#10005;</div>
+      <div className="remove-button" onClick={() => clearItem(id)}>&#10005;</div>
     </div >
   )
 };
 
-export default CheckoutItem;
+const mapDispatchToProps = dispatch => ({
+  addItem: item => dispatch(addItem(item)),
+  clearItem: id => dispatch(clearItem(id)),
+  removeItem: item => dispatch(removeItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -8,3 +8,14 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
   }
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
 }
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  return cartItems.map(cartItem => cartItem.id === cartItemToRemove.id
+    ? { ...cartItem, quantity: cartItem.quantity - (cartItem.quantity === 1 ? 0 : 1) }
+    : cartItem
+  )
+}
+
+
+
+
