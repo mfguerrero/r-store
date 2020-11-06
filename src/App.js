8686231d1861a09d09be7dfaac72583db576e0b9
Mario Feix <mfguerrero@msn.com>
2020-11-05 22:11:36 -0700

    checkout page and checkout item added

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 import HomePage from './pages/homepage/homepage.component';
 import SignInSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up.component';
 import ShopPage from './pages/shop/shop.component';
+import CheckoutPage from './pages/checkout/checkout.component';
 import Header from './components/header/header.component';
 import './App.css';
 
@@ -49,6 +50,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/shop" component={ShopPage} />
+          <Route exact path="/checkout" component={CheckoutPage} />
           <Route exact path="/signin" render={() => {
             return (this.props.currentUser ? <Redirect to="/" /> : <SignInSignUpPage />);
           }} />
--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,18 +1,29 @@
 import React from 'react'
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 
 import CartItem from '../cart-item/cart-item.component';
 import CustomButton from '../custom-button/custom-button.component';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
+import { toogleCartHidden } from '../../redux/cart/cart.actions'
 import './cart-dropdown.styles.scss';
 
-export const CartDropdown = ({ cartItems }) => {
+export const CartDropdown = ({ cartItems, history, dispatch }) => {
   return (
     <div className="cart-dropdown">
-      <div className="cart-items">
-        {cartItems.map(item => (<CartItem key={item.id} item={item} />))}
-      </div>
-      <CustomButton>CHECKOUT</CustomButton>
+      {
+        cartItems.length ?
+          <div className="cart-items">
+            {cartItems.map(item => (<CartItem key={item.id} item={item} />))}
+          </div> :
+          <span className="empty-message">Your cart is empty</span>
+      }
+      <CustomButton onClick={() => {
+        history.push('/checkout')
+        dispatch(toogleCartHidden())
+      }}>
+        CHECKOUT
+      </CustomButton>
     </div>
   )
 }
@@ -21,4 +32,4 @@ const mapStateToProps = state => ({
   cartItems: selectCartItems(state)
 });
 
-export default connect(mapStateToProps)(CartDropdown);
+export default withRouter(connect(mapStateToProps)(CartDropdown));
